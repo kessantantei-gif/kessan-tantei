85fa6919b10b82498e3703b45fba9431f5df730b
kessantantei-gif <kessan.tantei@gmail.com>
2026-07-06 06:33:26 +0900

    Remove duplicate company compare floating button

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import FeedbackButton from "@/components/feedback-button";
 import SiteNav from "@/components/site-nav";
 import CompanyTrendChartEnhancer from "@/components/company-trend-chart-enhancer";
 import ScoreExplanationInjector from "@/components/score-explanation-injector";
-import CompanyCompareFloatingButton from "@/components/company-compare-floating-button";
 import CompareTray from "@/components/compare-tray";
 
 export const metadata: Metadata = {
@@ -60,7 +59,6 @@ export default function RootLayout({
           {children}
           <CompanyTrendChartEnhancer />
           <ScoreExplanationInjector />
-          <CompanyCompareFloatingButton />
           <CompareTray />
           <FeedbackButton />
           <AuthButton />
