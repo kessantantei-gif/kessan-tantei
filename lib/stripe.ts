2e58be7ddb0ca2ad9deab532ae53901471b26f70
kessantantei-gif <kessan.tantei@gmail.com>
2026-07-06 21:02:14 +0900

    Avoid Stripe import-time env crash during deployment

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,23 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("STRIPE_SECRET_KEY が設定されていません");
+let cachedStripe: Stripe | null = null;
+
+export function getStripe() {
+  const secretKey = process.env.STRIPE_SECRET_KEY;
+
+  if (!secretKey) {
+    throw new Error("STRIPE_SECRET_KEY が設定されていません");
+  }
+
+  if (!cachedStripe) {
+    cachedStripe = new Stripe(secretKey);
+  }
+
+  return cachedStripe;
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+export const stripe = new Proxy({} as Stripe, {
+  get(_target, prop) {
+    return Reflect.get(getStripe(), prop);
+  },
+});
