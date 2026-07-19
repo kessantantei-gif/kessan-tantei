bda2234eba6830cb5343c53e37ae762d6df9ca8d
kessantantei-gif <kessan.tantei@gmail.com>
2026-07-19 21:08:03 +0900

    Revert redirect loop change

--- proxy.ts
+++ proxy.ts
@@ -1,27 +1,18 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+const VERCEL_PRODUCTION_HOST = "kessan-tantei.vercel.app";
 const CANONICAL_HOST = "kessan-tantei.jp";
-const REDIRECT_HOSTS = new Set([
-  "www.kessan-tantei.jp",
-  "kessan-tantei.vercel.app",
-]);
 
 export default clerkMiddleware((_auth, request) => {
   const forwardedHost = request.headers.get("x-forwarded-host");
-  const requestHost = (
-    forwardedHost ??
-    request.headers.get("host") ??
-    request.nextUrl.host
-  )
+  const requestHost = (forwardedHost ?? request.headers.get("host") ?? request.nextUrl.host)
     .split(",")[0]
     .trim()
     .toLowerCase()
     .replace(/:\d+$/, "");
 
-  if (!REDIRECT_HOSTS.has(requestHost)) {
-    return NextResponse.next();
-  }
+  if (requestHost !== VERCEL_PRODUCTION_HOST) return;
 
   const canonicalUrl = request.nextUrl.clone();
   canonicalUrl.protocol = "https:";
